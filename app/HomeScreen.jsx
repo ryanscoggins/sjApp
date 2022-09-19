2abeb3856d3431af9a34dfcf64e546a413c12f5f
Ryan Scoggins <ryanscoggins31@gmail.com>
2022-09-19 10:20:59 -0500

    formatting plus first text box

--- app/HomeScreen.jsx
+++ app/HomeScreen.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-	StyleSheet, Text, View,
+	StyleSheet, Text, View, TextInput,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const styles = StyleSheet.create({
 	container: {
@@ -11,12 +12,32 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
+	text: {
+		fontSize: '25',
+	},
+	box: {
+		fontSize: '25',
+	},
 });
 
 export default function App() {
+	// const initialDate = new Date(new Date().setHours(0, 0, 0, 0));
+	// const [mode, setMode] = useState('date');
+	// const onChange = (event, selectedDate) => {
+	//     console.log(selectedDate);
+	//   };
 	return (
 		<View style={styles.container}>
-			<Text>Here is the beginning of SJ's app!</Text>
+			<Text style={styles.text}>Here is the beginning of SJ's app!</Text>
+			<TextInput
+				style={styles.box}
+				value="1"
+			/>
+			{/* <DateTimePicker
+				value={initialDate}
+				mode="time"
+				onChange={onChange}
+			/> */}
 			<StatusBar style="auto" />
 		</View>
 	);
