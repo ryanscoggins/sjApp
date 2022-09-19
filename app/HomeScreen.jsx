a4e26cc029513169cba8a137118187631f86c087
Ryan Scoggins <ryanscoggins31@gmail.com>
2022-09-19 09:47:13 -0500

    lint

--- app/HomeScreen.jsx
+++ app/HomeScreen.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import {
-    StyleSheet, Text, View
+	StyleSheet, Text, View,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-    return (
-      <View style={styles.container}>
-        <Text>Here is the beginning of SJ's app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Here is the beginning of SJ's app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
